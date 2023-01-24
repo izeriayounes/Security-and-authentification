1aa9a4d86ead5d4c9c27941408a54bcbf56afc1f
izeriayounes <younesizeria@gmail.com>
2023-01-24 11:33:04 +0100

    implemented hashing to passwords

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const ejs = require('ejs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5')
 
 const app = express();
 
@@ -19,8 +19,6 @@ const userSchema = new mongoose.Schema({  // we used to create a simple js objec
     email: String,
     password: String
 })
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User =  mongoose.model('user', userSchema)
 
@@ -43,7 +41,7 @@ app.get('/logout', function(req, res){
 app.post('/register', function(req, res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password) 
     })
     User.findOne({email: req.body.username}, function(err, foundEmail){
         if (foundEmail){
@@ -58,7 +56,7 @@ app.post('/register', function(req, res){
 
 app.post('/login', function(req, res){
     email = req.body.username;
-    password = req.body.password;
+    password = md5(req.body.password);
     cnxToDB;
     User.findOne({email: email}, function(er, foundUser){
         if(!foundUser){
