57b7d5549efaa9acb0a4755476c4f973d46c2b74
izeriayounes <izeria.younes@ine.inpt.ma>
2023-05-19 11:04:21 +0100

    improve secrets page ui

--- models/Secret.js
+++ models/Secret.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const moment = require('moment');
 
+const currentDate = moment().format('YYYY-MM-DD');
+
 const secretSchema = new mongoose.Schema({
     date: {
         type: Date,
